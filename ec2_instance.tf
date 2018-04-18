923aeabeb30cb37eb6871eb66df6165ad04a085b
manee2k6 <manee2k6@gmail.com>
2018-04-18 09:44:31 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -7,7 +7,7 @@ provider "aws" {
    resource "aws_instance" "web" {
         ami = "ami-916f59f4"
         instance_type = "t2.micro"
-       # key_name = "padmavathy"
+        key_name = "${var.key_name}"
         security_groups= ["JenkinsSG"]
         tags {
          Name = "terraform-instance"
